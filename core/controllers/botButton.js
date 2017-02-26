472429ce7c000595f9ee968abb723dc565a6d1ac
fkapitalism <guilherme@unixlike.private>
2017-02-26 17:26:57 -0300

    Bot button modified to open and close the bot

--- core/controllers/botButton.js
+++ core/controllers/botButton.js
@@ -3,6 +3,11 @@ function botButton(){
 
 	//Show command panel
 	document.getElementById(BOT_BUTTON_DOM_ID).addEventListener("click", function(){
-		controllers.functions.resetBotAndShowPanel()
+		if(controllers.bot.controlPanel.isHidden)
+			controllers.functions.resetBotAndShowPanel()
+		else {
+			controllers.bot.controlPanel.isHidden = true
+			controllers.functions.hidePanel()
+		}
 	})
 }
