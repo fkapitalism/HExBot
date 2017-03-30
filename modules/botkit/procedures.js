9e903a06c2f0e7af90ea08e8b2075e9289d3dd50
Guilherme <gresendesa@gmail.com>
2017-03-30 00:35:32 -0300

    Fixing the reloading recaptcha page problem

--- modules/botkit/procedures.js
+++ modules/botkit/procedures.js
@@ -1,6 +1,15 @@
 var foo = $jSpaghetti.module("botkit")
 foo.config.debugMode = true
 
+foo.procedure("there_is_captcha", function(){
+	var frames = document.getElementsByTagName("iframe")
+	for (var i = 0; i < frames.length; i++) {
+		if (frames[i].title == "recaptcha widget")
+			return true
+	}
+	return false
+})
+
 foo.procedure("handle_cf_page", function(){
 	if(document.getElementById("cf-error-details")){
 		setTimeout(function() {
--- modules/botkit/sequences/run.js
+++ modules/botkit/sequences/run.js
@@ -1,6 +1,7 @@
 var foo = $jSpaghetti.module("botkit").sequence("run")
 
 foo.instructions = [
-	{0: ["handle_cf_page", "_exit"]} 
+	{"@init": ["there_is_captcha", {"gotoif":["*.$", "@finish"]}, "handle_cf_page"]},
+	{"@finish": ["_exit"]}
 ]
 
